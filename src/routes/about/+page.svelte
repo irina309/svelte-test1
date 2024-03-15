e066bedbfb186fd934c3e3a20ee05f80561ff526
Irina <leonteva.irina309@mail.ru>
2024-03-15 08:25:18 +0300

    chage page1

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,10 @@
+
+<h1>Обо мне</h1>
+
+ 
+ <h2 class='subtitle'>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi cupiditate adipisci voluptatum quaerat
+    dicta hic perferendis soluta vitae maxime deleniti, quae nulla animi voluptas velit est ipsa accusantium 
+    deserunt alias?
+</h2>
+    
