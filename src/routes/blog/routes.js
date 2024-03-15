9f54a3785a74e897206638210b47ac0b3927f11c
Irina <leonteva.irina309@mail.ru>
2024-03-15 11:21:56 +0300

    add new page

--- src/routes/blog/routes.js
+++ src/routes/blog/routes.js
@@ -2,6 +2,6 @@ export const routes = [
     {
         id: 1,
         title: 'Ve First Blog Post',
-        body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi cupiditate adipisci voluptatum quaera dicta hic perferendis soluta vitae maxime deleniti, quae nulla animi voluptas velit est ipsa accusantium'
+        body: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi cupiditate adipisci voluptatum quaera dicta hic perferendis soluta vitae maxime deleniti, quae nulla animi voluptas velit est ipsa accusantium',
     }
-]
+];
